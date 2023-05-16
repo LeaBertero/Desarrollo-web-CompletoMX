d7681395e196f2ae57fbf17e00e88670a6ff1ee9
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-16 17:08:54 -0300

    object metods

---
+++ MODULO-JAVASCRIPT/Destructurin-objetos/JavaScript/Destructuring-objetos.js
@@ -0,0 +1,29 @@
+
+const producto = {
+    nombreProducto: 'monitor 20 pulgadas',
+    precio: 300,
+    dispinible: true,
+}
+
+// const precioProducto = producto.precio;
+// const nombreProducto = producto.nombreProducto;
+
+// console.log(precioProducto);
+// console.log(nombreProducto);
+
+
+//Destructuring Objetos
+
+const {precio} = producto;
+const{ nombreProducto } = producto;
+
+
+console.log(precio);
+console.log(nombreProducto);
+
+
+
+
+
+
+
---
+++ MODULO-JAVASCRIPT/Modificar-objetos/JavaScript/moificar-objetos.js
@@ -0,0 +1,21 @@
+//Modificar objetos
+
+//objetos
+const producto = {
+    nombreProducto : 'Monitor 20 pulgadas',
+    precio: 300,
+    disponible: true,
+}
+
+
+//agregar nuevas propiedades
+producto.imagen = 'imagen.jpg';
+
+
+
+// delete producto.dispinible;
+console.log(producto);
+
+
+
+
