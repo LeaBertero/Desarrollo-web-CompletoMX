a3f7884c0b1ad39efa9bc17ae60fd2ca15a654c4
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-16 02:40:08 -0300

    metodos para los strings

---
+++ MODULO-JAVASCRIPT/Variables-Let-Y-Const/JavaScript/Let-&-Const.js
@@ -0,0 +1,17 @@
+// variables con let
+
+let producto = 'audifonos gamer';
+
+let disponible;
+
+disponible = true;
+
+let producto1 = 'computadora',
+disponible1 = true,
+categoria = 'computadoras';
+
+
+let disponible_;
+
+let nombreProducto = 'Monitor HD';
+
---
+++ MODULO-JAVASCRIPT/strings-o-cadenas-de-texto/javascript/cadenas-texto.js
@@ -0,0 +1,10 @@
+//string o cadenas de texto
+
+const producto = 'monitor de 20 pulgadas';
+
+console.log(producto);
+
+
+ 
+
+
