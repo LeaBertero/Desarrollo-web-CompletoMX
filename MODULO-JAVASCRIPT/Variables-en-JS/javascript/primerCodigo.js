a7f95db29f4ac01c2910fbdb33a828f8e25aad27
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-16 02:13:21 -0300

    variables con let y const

---
+++ MODULO-JAVASCRIPT/Variables-en-JS/javascript/primerCodigo.js
@@ -0,0 +1,21 @@
+//variables
+
+var producto = 'audifonos gamer'; //iniciar variable y darle valor
+
+//Recordatorio importante: no usar var
+var disponible; //iniciamos la variable , pero sin valor
+
+// prodcuto = true; // reasignando el valor de la variable
+
+disponible = true;
+
+
+//definir varias variables de una sola vez
+var producto1 = 'computadoras',
+disponible = true,
+categorias = 'computadoras';
+
+console.log(producto);
+
+
+
