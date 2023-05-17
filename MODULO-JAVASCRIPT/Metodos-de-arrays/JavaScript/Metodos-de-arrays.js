a6d180fcf58f01cc8600bd92a06c0633d35ad18e
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-17 02:36:24 -0300

    Métodos de arrays

---
+++ MODULO-JAVASCRIPT/Metodos-de-arrays/JavaScript/Metodos-de-arrays.js
@@ -0,0 +1,14 @@
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+
+const carrito = [
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Monitor 20 pulgadas', precio: 500},
+];
