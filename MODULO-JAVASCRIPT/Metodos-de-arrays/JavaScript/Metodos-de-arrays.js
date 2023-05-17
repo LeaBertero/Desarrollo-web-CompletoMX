a8f6ca6e14e223dd803db8f6fefcc672ec97a42b
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-17 11:21:03 -0300

    Metodos y arrays con aspiracion a las funciones flecha

--- MODULO-JAVASCRIPT/Metodos-de-arrays/JavaScript/Metodos-de-arrays.js
+++ MODULO-JAVASCRIPT/Metodos-de-arrays/JavaScript/Metodos-de-arrays.js
@@ -3,12 +3,22 @@ const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
 
 const carrito = [
     {nombre: 'Monitor 20 pulgadas', precio: 500},
-    {nombre: 'Monitor 20 pulgadas', precio: 500},
-    {nombre: 'Monitor 20 pulgadas', precio: 500},
-    {nombre: 'Monitor 20 pulgadas', precio: 500},
-    {nombre: 'Monitor 20 pulgadas', precio: 500},
-    {nombre: 'Monitor 20 pulgadas', precio: 500},
+    {nombre: 'Television 50 pulgadas', precio: 700},
+    {nombre: 'Tablet', precio: 300},
+    {nombre: 'Audifonos', precio: 200},
+    {nombre: 'Teclado', precio: 50},
+    {nombre: 'Celular', precio: 500},
     {nombre: 'Monitor 20 pulgadas', precio: 500},
     {nombre: 'Monitor 20 pulgadas', precio: 500},
     {nombre: 'Monitor 20 pulgadas', precio: 500},
 ];
+
+meses.forEach(function(mes){
+    if (mes == 'Marzo') {
+        console.log('Marzo si existe');
+    }
+});
+
+const resultado = meses.includes('Marzo');
+console.log(resultado);
+        
