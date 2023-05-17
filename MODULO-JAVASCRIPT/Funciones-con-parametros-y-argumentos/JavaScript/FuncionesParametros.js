0f94f21ab69b3bdf97ce93e55e3ef5a62db0f765
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-17 15:00:46 -0300

    Funciones con parametros y argumentos

---
+++ MODULO-JAVASCRIPT/Funciones-con-parametros-y-argumentos/JavaScript/FuncionesParametros.js
@@ -0,0 +1,16 @@
+//Funciones con parametros y argumentos
+function sumar(numer1, numero2){ //numero1 y numero2 son parametros
+    console.log(numer1 + numero2);
+}
+sumar(10,10); //argumentos o valores reales
+sumar(10,20); //argumentos o valores reales
+sumar(10,30); //argumentos o valores reales
+sumar(10,50); //argumentos o valores reales
+
+const sumar2 = function(){
+    console.log(3 + 3);
+}
+
+sumar2();
+
+
