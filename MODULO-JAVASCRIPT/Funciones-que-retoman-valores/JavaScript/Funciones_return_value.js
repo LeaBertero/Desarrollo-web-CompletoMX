bbb3a5c4fb02339f53d39f368a502a8d0cc45caf
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-18 04:58:27 -0300

    carrito de compras

--- MODULO-JAVASCRIPT/Funciones-que-retoman-valores/JavaScript/Funciones_return_value.js
+++ MODULO-JAVASCRIPT/Funciones-que-retoman-valores/JavaScript/Funciones_return_value.js
@@ -1,2 +1,34 @@
 //FUNCIONES QUE RETOMAN VALORES
 
+// function sumar(n1,n2){
+//     return n1 + n2;
+// }
+
+// const resultado = sumar(2,3);
+
+// console.log(resultado);
+
+
+//CARRITO DE COMPRAS
+
+let total = 0;
+
+function agregarCarrito(precio){
+    return total += precio;
+}
+
+function calcularImpuesto(total){
+    return 1.15 * total;
+}
+
+total = agregarCarrito(200);
+total = agregarCarrito(400);
+total = agregarCarrito(600);
+
+
+console.log(total);
+
+
+const totalAPagar = calcularImpuesto(total);
+
+console.log(`El total a  pagar con impuestos es de: $${totalAPagar}`);
