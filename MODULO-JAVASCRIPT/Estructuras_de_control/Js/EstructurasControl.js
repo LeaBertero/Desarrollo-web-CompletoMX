4a638fbf5e4c5b5db5587bccef4ee779257f9a22
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-19 13:37:05 -0300

    swuitch - (los case del swuitch no funcionan como deberian)

---
+++ MODULO-JAVASCRIPT/Estructuras_de_control/Js/EstructurasControl.js
@@ -0,0 +1,7 @@
+const puntaje = 100;
+
+if (puntaje !== 1000){
+    console.log('No es igual');
+}else{
+    console.log('Si el puntaje es 1000');
+}
---
+++ MODULO-JAVASCRIPT/Switch/JavaScript/Switch.js
@@ -0,0 +1,19 @@
+const metodoPago = 'cheque';
+
+switch (metodoPago) {
+    case 'tarjeta':
+        console.log('Pagaste con tarjeta');
+        break;
+
+    case 'Cheque':
+        console.log('El usuario va a pagar con cheque, revisaremos los fondos primero');
+        break;
+    case 'efectivo':
+        console.log('Pagaste con efectivo');
+        break;
+
+    default:
+        console.log('Aun no has pagado');
+        break;
+
+}
