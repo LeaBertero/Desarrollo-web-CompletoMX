de3482a1e5a119e7ef094cda1308156d4a5ecd69
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-19 11:46:17 -0300

    Estructuras de control

--- MODULO-JAVASCRIPT/ArrowFunctions/JavaScript/ArrowFunctions.js
+++ MODULO-JAVASCRIPT/ArrowFunctions/JavaScript/ArrowFunctions.js
@@ -1,4 +1,18 @@
 //arrow functions o funciones flecha
 
 
+const sumar2 = (n1,n2) => console.log(n1 + n2);
+
+sumar2(5,10);
+
+const aprendiendo = (tecnologia) => {
+    console.log('Aprendiendo tecnologia');
+}
+
+aprendiendo('JavaScript');
+
+
+
+
+
 
