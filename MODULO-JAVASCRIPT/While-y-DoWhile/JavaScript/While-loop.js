f4a353e568e9ba96f96b7fceaa1050bb6c36db09
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-20 09:51:30 -0300

    while loop con incremento en 1 funciona correcto

--- MODULO-JAVASCRIPT/While-y-DoWhile/JavaScript/While-loop.js
+++ MODULO-JAVASCRIPT/While-y-DoWhile/JavaScript/While-loop.js
@@ -11,3 +11,19 @@ while (incremento < 10){ //condicion
 }
 
 //realizando una prueba de push
+
+//while loop
+
+let incremento2 = 1;
+
+while(incremento2 <= 100){
+    
+    if(incremento2 % 2 === 0){
+        console.log(`El numero ${incremento2} es par`);
+        
+    } 
+
+
+
+    incremento2++;
+}
