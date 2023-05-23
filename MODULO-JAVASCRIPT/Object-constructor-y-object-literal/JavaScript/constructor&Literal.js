fb30814b446536e5c5924486ed73bc5ab2104104
Leandro Bertero <leandro.bertero@hotmail.com>
2023-05-23 03:55:00 -0300

    objeto constructor y objeto literal

---
+++ MODULO-JAVASCRIPT/Object-constructor-y-object-literal/JavaScript/constructor&Literal.js
@@ -0,0 +1,31 @@
+// alert('Hola')
+
+
+//objeto literal
+// const producto = {
+//     nombre: 'Tablet',
+//     precio: 500,
+
+// }
+
+
+//objeto constructor
+function producto(nombre, precio){
+    this.nombre = nombre;
+    // this.precio = precio;
+}
+
+const producto2 = new producto("Monitor curvo 50 pulgadas", 800);
+console.log(producto2);
+
+
+//crear dos productos con diferentes valores como ejercicio en js
+
+function perro(raza, color){
+    
+}
+
+const perro =
+
+console.log(producto2);
+
--- MODULO-JAVASCRIPT/This-en-JavaScript/JavaScript/This.js
+++ MODULO-JAVASCRIPT/This-en-JavaScript/JavaScript/This.js
@@ -1,3 +1,34 @@
 // alert('Hola mundo');
 //Ejemplo de This en JavaScript
 
+
+//objeto = object literal
+//template string ${ espacio donde se puede conbonar variables con strings }
+
+const reservacion = {
+    nombre: 'Leandro',
+    apellido: 'Bertero',
+    total: 5000,
+    pagado: false,
+    informacion: function(){
+        console.log(`El cliente ${this.nombre}, reservó y su cantidad a pagar es de ${this.total}`);
+    }
+}
+
+
+const reservacion2 = {
+    nombre: 'Pedro',
+    apellido: 'Bertero',
+    total: 5000,
+    pagado: false,
+    informacion2: function(){
+        console.log(`El cliente ${this.nombre}, reservó y su cantidad a pagar es de ${this.total}`);
+    }
+}
+
+
+
+console.log( reservacion.informacion() );
+console.log( reservacion2.informacion2() );
+    
+       
